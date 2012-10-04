237561b3b9146706fe37c0334320bdabf9e71814
Hernan Grecco <hernan.grecco@gmail.com>
2012-10-05 00:56:58 +0200

    Add a command line parameter to the System example to print details

--- examples/nidaqmx/system.py
+++ examples/nidaqmx/system.py
@@ -3,13 +3,14 @@
 
 from lantz.drivers.ni.daqmx import System
 
-def bulleted_list(title, elements, indent=2, bullet='-'):
+def bulleted_list(title, elements, indent=2, bullet='-', details=False):
     elements = tuple(elements)
     sp = ' ' * indent
     print('{}{} {}: {}'.format(sp, bullet, title, len(elements)))
-    sp += ' '
-    for element in elements:
-        print(' {}{} {}'.format(sp, bullet, element))
+    if details:
+        sp += ' '
+        for element in elements:
+            print(' {}{} {}'.format(sp, bullet, element))
 
 if __name__ == '__main__':
     import argparse
@@ -17,6 +18,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Print debug information')
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='List details')
 
     args = parser.parse_args()
 
@@ -37,22 +40,22 @@ if __name__ == '__main__':
         print(' - Product Type: {}'.format(value.product_type))
         print(' - Serial Number: {}'.format(value.serial_number))
         print(' - Bus Info: {}'.format(value.bus_info))
-        bulleted_list('AI Channels', value.analog_input_channels)
-        bulleted_list('AI Channels', value.analog_input_channels)
-        bulleted_list('AO Channels', value.analog_output_channels)
-        bulleted_list('DI Lines', value.digital_input_lines)
-        bulleted_list('DO Lines', value.digital_output_lines)
-        bulleted_list('DI Ports', value.digital_input_ports)
-        bulleted_list('DI Ports', value.digital_output_ports)
-        bulleted_list('CI Ports', value.counter_input_channels)
-        bulleted_list('CO Ports', value.counter_output_channels)
+        bulleted_list('AI Channels', value.analog_input_channels, details=args.list)
+        bulleted_list('AI Channels', value.analog_input_channels, details=args.list)
+        bulleted_list('AO Channels', value.analog_output_channels, details=args.list)
+        bulleted_list('DI Lines', value.digital_input_lines, details=args.list)
+        bulleted_list('DO Lines', value.digital_output_lines, details=args.list)
+        bulleted_list('DI Ports', value.digital_input_ports, details=args.list)
+        bulleted_list('DI Ports', value.digital_output_ports, details=args.list)
+        bulleted_list('CI Ports', value.counter_input_channels, details=args.list)
+        bulleted_list('CO Ports', value.counter_output_channels, details=args.list)
     print('')
     print('System Tasks: {}'.format(len(inst.tasks)))
     for key, value in inst.tasks.items():
         print('-- {} --'.format(key))
         print(' - Type: {}'.format(value.io_type))
         print(' - Task Complete: {}'.format(value.is_done))
-        bulleted_list('Devices', value.devices.keys())
-        bulleted_list('Channels', value.channels.keys())
+        bulleted_list('Devices', value.devices.keys(), details=args.list)
+        bulleted_list('Channels', value.channels.keys(), details=args.list)
     print('')
     print('System Channels: {}'.format(len(inst.channels)))
