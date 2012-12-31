52053aaf72ce3affa2f69f8f01cc91aeb7d0156d
Hernan Grecco <hernan.grecco@gmail.com>
2012-12-31 17:54:34 +0100

    Added short title to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -118,7 +118,7 @@ html_theme_path = ['_themes']
 html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'Lantz'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
