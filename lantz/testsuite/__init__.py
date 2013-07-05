900ce219dd6839c5b2b74e67a27c2a02c8ec6481
Hernan Grecco <hernan.grecco@gmail.com>
2013-07-05 19:36:22 -0300

    Move test suite inside lantz package.

---
+++ lantz/testsuite/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import logging
+import unittest
+
+
+def testsuite():
+    """A testsuite that has all the pyflim tests.
+    """
+    return unittest.TestLoader().discover(os.path.dirname(__file__))
+
+
+def main():
+    """Runs the testsuite as command line application."""
+    try:
+        unittest.main()
+    except Exception as e:
+        print('Error: %s' % e)
