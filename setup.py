4fcab93c51a13b9edefb45eafa23d0a1e331b919
Hernan Grecco <hernan.grecco@gmail.com>
2014-01-06 14:05:07 -0300

    Updated compatibility list

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(name='Lantz',
            'Programming Language :: Python',
            'Programming Language :: Python :: 3.2',
            'Programming Language :: Python :: 3.3',
+           'Programming Language :: Python :: 3.4',
            'Topic :: Scientific/Engineering',
            'Topic :: Software Development :: Libraries'
       ],
