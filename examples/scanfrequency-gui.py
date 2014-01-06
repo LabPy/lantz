316daba7d359863edd5e301d49f5aad413a46ee4
Hernan Grecco <hernan.grecco@gmail.com>
2014-01-06 04:05:27 -0300

    Added the examples from the tutorial

---
+++ examples/scanfrequency-gui.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+    scanfrequency
+    ~~~~~~~~~~~~~
+
+    This example shows how to program a GUI using Qt and lantz drivers, but
+    without the backend-frontend classes that simplifies app development.
+
+    :copyright: 2014 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import sys
+
+from lantz import Q_
+
+# Import from lantz a function to connect drivers to UI
+from lantz.ui.widgets import connect_driver
+
+# Import lantz.ui register an import hook that will replace calls to Qt by PyQt4 or PySide ...
+# and here we just use Qt and will work with both bindings!
+from lantz.utils.qt import QtGui, QtCore
+
+# These imports are from your own project
+from dummydrivers import SomeFGDriver
+
+# And we reuse the code from the command line application.
+from scanfrequency import scan_frequency
+
+qapp = QtGui.QApplication(sys.argv)
+
+# Load the UI from the QtDesigner file. You can also use pyuic4 to generate a class.
+main = QtGui.loadUi('scanfrequency.ui')
+
+Hz = Q_(1, 'Hz')
+sec = Q_(1, 'sec')
+
+with SomeFGDriver() as inst:
+
+    # Connect the main panel widgets to the instruments Feats,
+    # matching by name
+    connect_driver(main, inst, prefix='fungen')
+
+    # Obtain a reference to the widgets controlling the scan parameters
+    start = main.findChild((QtGui.QWidget, ), 'start')
+    stop = main.findChild((QtGui.QWidget, ), 'stop')
+    step = main.findChild((QtGui.QWidget, ), 'step')
+    wait = main.findChild((QtGui.QWidget, ), 'wait')
+    scan = main.findChild((QtGui.QWidget, ), 'scan')
+    progress = main.findChild((QtGui.QWidget, ), 'progress')
+
+    def update_progress_bar(new, old):
+        fraction = (new.magnitude - start.value()) / (stop.value() - start.value())
+        progress.setValue(fraction * 100)
+
+    inst.frequency_changed.connect(update_progress_bar)
+
+    # <--------- New code--------->
+    # Define a function to read the values from the widget and call scan_frequency
+    class Scanner(QtCore.QObject):
+
+        def scan(self):
+            # Call the scan frequency
+            scan_frequency(inst, start.value() * Hz, stop.value() * Hz,
+                           step.value() * Hz, wait.value() * sec)
+            # When it finishes, set the progress to 100%
+            progress.setValue(100)
+
+    thread = QtCore.QThread()
+    scanner = Scanner()
+    scanner.moveToThread(thread)
+    thread.start()
+
+    # Connect the clicked signal of the scan button to the function
+    scan.clicked.connect(scanner.scan)
+
+    qapp.aboutToQuit.connect(thread.quit)
+    # <--------- End of new code --------->
+
+    main.show()
+    exit(qapp.exec_())
---
+++ examples/scanfrequency.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+    scanfrequency
+    ~~~~~~~~~~~~~
+
+    This example shows how to program a CLI using a lantz drivers, but
+    without the backend-frontend classes that simplifies app development.
+
+    :copyright: 2014 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import time
+
+
+def scan_frequency(inst, start, stop, step, wait):
+    """Scan frequency in an instrument.
+
+    :param start: Start frequency.
+    :type start: Quantity
+    :param stop: Stop frequency.
+    :type stop: Quantity
+    :param step: Step frequency.
+    :type step: Quantity
+    :param wait: Waiting time.
+    :type wait: Quantity
+
+    """
+    in_secs = wait.to('seconds').magnitude
+    current = start
+    while current < stop:
+        inst.frequency = current
+        time.sleep(in_secs)
+        current += step
+
+
+if __name__ == '__main__':
+    import argparse
+
+    from lantz import Q_
+
+    from dummydrivers import SomeFGDriver
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('start', type=float,
+                        help='Start frequency [Hz]')
+    parser.add_argument('stop', type=float,
+                        help='Stop frequency [Hz]')
+    parser.add_argument('step', type=float,
+                        help='Step frequency [Hz]')
+    parser.add_argument('wait', type=float,
+                        help='Waiting time at each step [s]')
+
+    args = parser.parse_args()
+
+    Hz = Q_(1, 'Hz')
+    sec = Q_(1, 'sec')
+
+    def print_change(new, old):
+        print('Changed from {} to {}'.format(old, new))
+
+    with SomeFGDriver() as inst:
+        print(inst.idn)
+
+        inst.frequency_changed.connect(print_change)
+
+        scan_frequency(inst, args.start * Hz, args.stop * Hz,
+                       args.step * Hz, args.wait * sec)
