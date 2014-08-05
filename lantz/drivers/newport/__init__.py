59e28a13355996365991d8d2eb9651ca4725d242
unknown <carattino@physics.ledienuniv.nl>
2014-08-05 17:44:05 +0200

    Created driver for NewPort powermeter 1830c.

---
+++ lantz/drivers/newport/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    lantz.drivers.tektronix
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    :company: Newport.
+    :description: Test and Measurement Equipment.
+    :website: http://www.newport.com/
+
+    ---
+
+    :copyright: 2012 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD,
+
+"""
+
+from .powermeter1830c import powermeter1830c
+
+__all__ = ['powermeter1830c']
