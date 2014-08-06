98cbc9f7cba524163e71b0ad753cff7df2b06bb3
Hernan Grecco <hernan.grecco@gmail.com>
2014-08-06 10:51:24 -0300

    Added standford

---
+++ lantz/drivers/stanford/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    lantz.drivers.stanford
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    :company: Standford Research Systems.
+    :description: Manufactures test instruments for research and industrial applications
+    :website: http://www.thinksrs.com/
+
+    ----
+
+    :copyright: 2012 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from .minilasevo import MiniLasEvo
+
+__all__ = ['MiniLasEvo']
